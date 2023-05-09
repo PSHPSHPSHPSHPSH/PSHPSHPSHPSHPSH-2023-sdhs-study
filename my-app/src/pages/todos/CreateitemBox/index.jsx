dbdc3ffd9b32cccc4f9b9aad31566356d04f124d
박신호 <88702211+PSHPSHPSHPSHPSH@users.noreply.github.com>
2023-05-09 10:39:18 +0900

    3

---
+++ my-app/src/pages/todos/CreateitemBox/index.jsx
@@ -0,0 +1,11 @@
+import * as S from './styled';
+
+function CreateItemBox() {
+    return (
+        <S.InputBox>
+            <S.Input placeholder='입력' ></S.Input> 
+        </S.InputBox>
+    );
+}
+
+export default CreateItemBox;
---
+++ my-app/src/pages/todos/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Button = styled.div`
+  display: flex;
+  flex-directionL column;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h2``;
+
+export const InputBox = styled.div`
+  display: flex'
+  width: 100#;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 4px;
+`;
