83a2919b5a249979941b5de6ee0abe398c42ec23
박신호 <88702211+PSHPSHPSHPSHPSH@users.noreply.github.com>
2023-05-09 09:23:32 +0900

    scss
    scss

---
+++ my-app/src/pages/main/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  alingn-item: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
